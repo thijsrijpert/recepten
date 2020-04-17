5695d1278c0ec8f9ba4d1c23d159196cf68c7076
SavingPrivateHenk <61140909+SavingPrivateHenk@users.noreply.github.com>
2020-04-17 16:49:32 +0200

    addReligie update

--- android/BeroepsproductBlok4RecipesWorldwide/app/src/main/java/com/nl/beroepsproductblok4_recipesworldwide/religie/AddReligie.java
+++ android/BeroepsproductBlok4RecipesWorldwide/app/src/main/java/com/nl/beroepsproductblok4_recipesworldwide/religie/AddReligie.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.nl.beroepsproductblok4_recipesworldwide.R;
-import com.nl.beroepsproductblok4_recipesworldwide.administrator.tijdvakken.AddTijdvak_WebserverConnector;
+import com.nl.beroepsproductblok4_recipesworldwide.administrator.tijdvakken.AddReligie_WebserverConnector;
 
 import java.util.ArrayList;
 
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 public class AddReligie extends Fragment {
     private View view;
     private EditText edittext_religieName;
-    private AddTijdvak_WebserverConnector addTijdvak_webserverConnector;
+    private AddReligie_WebserverConnector addReligie_webserverConnector;
 
     public AddReligie() {
         // Required empty public constructor
@@ -32,13 +32,13 @@ public class AddReligie extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_add_tijdvak, container, false);
+        view = inflater.inflate(R.layout.fragment_add_religie, container, false);
 
         // Initialize the Class variables
-        edittext_religieName = view.findViewById(R.id.addTijdvak_edittext_tijdvakName);
+        edittext_religieName = view.findViewById(R.id.addReligie_edittext_religieName);
 
         // Create the connector that will pass requests towards the database
-        addTijdvak_webserverConnector = new AddTijdvak_WebserverConnector(this.getContext(), view);
+        addReligie_webserverConnector = new AddReligie_WebserverConnector(this.getContext(), view);
 
         // Launch the initialization methods
         initializeButtons();
@@ -47,27 +47,27 @@ public class AddReligie extends Fragment {
     }
 
     private void initializeButtons() {
-        Button button_applyTijdvak = view.findViewById(R.id.addTijdvak_btn_applyTijdvak);
-        button_applyTijdvak.setOnClickListener(new View.OnClickListener() {
+        Button button_applyReligie = view.findViewById(R.id.addReligie_btn_applyReligie);
+        button_applyReligie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // First, check if all the fields are filled in. If not, display a Toast accordingly
-                ArrayList<String> arraylist_tijdvakken = addTijdvak_webserverConnector.getAllTijdvakken();
+                ArrayList<String> arraylist_religie = addReligie_webserverConnector.getAllReligie();
                 if (edittext_religieName.getText().toString().equals("")) {
                     // Check if a name is entered
-                    Toast.makeText(getActivity(), "U moet een tijdvak naam invullen", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "U moet een religie naam invullen", Toast.LENGTH_SHORT).show();
                     return;
                 } else {
                     // Check for duplicates
-                    for (int c = 0; c < arraylist_tijdvakken.size(); c++) {
-                        if (arraylist_tijdvakken.get(c).equals(edittext_religieName.getText().toString())) {
-                            Toast.makeText(getActivity(), "Het ingevulde tijdvak naam bestaat al", Toast.LENGTH_SHORT).show();
+                    for (int c = 0; c < arraylist_religie.size(); c++) {
+                        if (arraylist_religie.get(c).equals(edittext_religieName.getText().toString())) {
+                            Toast.makeText(getActivity(), "Het ingevulde religie naam bestaat al", Toast.LENGTH_SHORT).show();
                             return;
                         }
                     }
                 }
 
-                boolean value = addTijdvak_webserverConnector.addTijdvak(edittext_religieName.getText().toString());
+                boolean value = addReligie_webserverConnector.addTijdvak(edittext_religieName.getText().toString());
 
                 if (value) {
                     edittext_religieName.setText("");
