ef1ef450529dc3f9c7b9b00d56e32de85137fb55
Thijs Rijpert <60793766+thijsrijpert@users.noreply.github.com>
2020-04-22 11:30:40 +0200

    Again some minor fixes to religion

--- server/api/Religion.php
+++ server/api/Religion.php
@@ -1,18 +1,8 @@
 <?php
 namespace api;
-<<<<<<< Updated upstream
 require_once('../model/Religion.php');
 require_once('../database/Religion.php');
 require_once('../exception/NullPointerException.php');
-=======
-echo 'test';
-require_once('../model/Religion.php');
-echo 'test';
-require_once('../database/Religion.php');
-echo 'test3';
-require_once('../exception/NullPointerException.php');
-echo 'test4';
->>>>>>> Stashed changes
 require_once('Api.php');
 
 class Religion extends Api{
@@ -21,24 +11,14 @@ class Religion extends Api{
 
     function __construct(){
         parent::__construct();
-<<<<<<< Updated upstream
-        //set_error_handler('error_handler');
-=======
         set_error_handler(array($this, 'error_handler'));
->>>>>>> Stashed changes
     }
 
     function insert() : void{
         try{
-<<<<<<< Updated upstream
-            $this->model = new model\Religion($_GET['name']);
-
-            $religieStatement = new database\Religion();
-=======
             $this->model = new \model\Religion($_GET['name']);
 
             $religieStatement = new \database\Religion();
->>>>>>> Stashed changes
             $code = $religieStatement->insert($this->model);
 
             $code = substr($code, 0, 2);
@@ -47,26 +27,13 @@ class Religion extends Api{
 
         }catch(\PDOException $e){
             parent::setHttpCode($e->getCode());
-<<<<<<< Updated upstream
-        }catch(exception\NullPointerException $e){
-            header('HTTP/1.0 400 Bad Request');
-=======
         }catch(\exception\NullPointerException $e){
             header('HTTP/1.0 400 Bad Request');
             restore_error_handler();
->>>>>>> Stashed changes
         }
     }
 
     function error_handler(){
-<<<<<<< Updated upstream
-        throw new exception\NullPointerException("Get value isn't passed");
-    }
-}
-
-$religie = new Religie();
-$religie->insert();
-=======
         if($errstr == 'Undefined index: name'){
             throw new \exception\NullPointerException("Get value isn't passed");
         }else{
@@ -77,5 +44,4 @@ $religie->insert();
 
 $religion = new Religion();
 $religion->insert();
->>>>>>> Stashed changes
 ?>
