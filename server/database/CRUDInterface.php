6c13dea17d3b1596dc09e3913ab11497c1597ea5
BramWildenberg <bram.wildenberg@rocdelijegraaf.nl>
2020-05-15 10:51:27 +0200

    changed returntype of function within CRUDINterface &

--- server/database/CRUDInterface.php
+++ server/database/CRUDInterface.php
@@ -2,5 +2,5 @@
 namespace database;
 require_once(dirname(__FILE__,1) . '/CRUInterface.php');
 interface CRUDInterface extends CRUInterface{
-    function delete(\model\Model $model) : String;
+    function delete(\model\Model $model) : Array;
 }
