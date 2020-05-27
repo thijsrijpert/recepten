4ea579d01d440727cf438c7a29ab850ec7739b08
SavingPrivateHenk <61140909+SavingPrivateHenk@users.noreply.github.com>
2020-05-27 17:03:18 +0200

    update connector

--- android/BeroepsproductBlok4RecipesWorldwide/app/src/main/java/com/nl/recipeapp/login/Connector.java
+++ android/BeroepsproductBlok4RecipesWorldwide/app/src/main/java/com/nl/recipeapp/login/Connector.java
@@ -1,4 +1,14 @@
 package com.nl.recipeapp.login;
 
+import android.content.Context;
+import android.view.View;
+
+import com.nl.recipeapp.R;
+
 public class Connector {
+
+    public Connector(Context context, View view) {
+
+    }
+
 }
