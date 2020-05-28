859b501adee4142a36e2df573cc20acb28cd29a4
SavingPrivateHenk <61140909+SavingPrivateHenk@users.noreply.github.com>
2020-05-28 12:10:05 +0200

    Update

--- android/BeroepsproductBlok4RecipesWorldwide/app/src/main/java/com/nl/recipeapp/login/Login.java
+++ android/BeroepsproductBlok4RecipesWorldwide/app/src/main/java/com/nl/recipeapp/login/Login.java
@@ -9,8 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nl.recipeapp.R;
 import com.nl.recipeapp.register.Register;
@@ -23,11 +26,15 @@ public class Login extends Fragment {
 
     View view;
 
+    private LinearLayout login_linearLayout_textviews;
+    private EditText edit_login, edit_password;
+
+        // Loads the register fragment when clicked on register textfield
     private Fragment fragment_register;
+
     public Login() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +51,10 @@ public class Login extends Fragment {
         transaction.replace(R.id.login_framelayout, fragment_register);
         transaction.commit();
 
+        // Initialize the Class variables
+        edit_login = view.findViewById(R.id.edit_login);
+        edit_password = view.findViewById(R.id.edit_password);
+
         // Create the textview which toggles the display of the framelayout
         TextView t2 = view.findViewById(R.id.login_textView_registratie);
         t2.setOnClickListener(new View.OnClickListener() {
@@ -51,13 +62,37 @@ public class Login extends Fragment {
             public void onClick(View v) {
                 if (frameLayout.getVisibility() == View.INVISIBLE) {
                     frameLayout.setVisibility(View.VISIBLE);
+
                 } else {
                     frameLayout.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        return view;
+        Button login_btn_signIn = view.findViewById(R.id.login_btn_signIn);
+        login_btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // First, check if edit_login is filled in. If not, display a Toast accordingly
+                if (edit_login.getText().toString().equals("")) {
+                    // Check if a name is entered
+                    Toast.makeText(getActivity(), "U moet een gebruikersnaam invullen", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
+                // Second, check if the password field in. If not, display a Toast accordingly
+                if (edit_password.getText().toString().equals("")) {
+                    Toast.makeText(getActivity(), "Uw moet uw wachtwoord invullen", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                else {
+                    edit_login.setText("");
+                    edit_password.setText("");
+                }
+            }
+        });
+
+        return view;
     }
 }
