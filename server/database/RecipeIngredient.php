8fc56640d58d4beaeb536bdbadebd9048e47934f
BramWildenberg <bram.wildenberg@rocdelijegraaf.nl>
2020-06-05 14:16:12 +0200

    fixed the insert for combo table

--- server/database/RecipeIngredient.php
+++ server/database/RecipeIngredient.php
@@ -2,9 +2,8 @@
 namespace database;
 require_once(dirname(__FILE__,1) . '/Database.php');
 require_once(dirname(__FILE__,1) . '/CRUD.php');
-require_once(dirname(__FILE__,1) . '/CRUInterface.php');
 require_once(dirname(__FILE__,2) . '/model/RecipeIngredient.php');
-class RecipeIngredient extends CRUD implements CRUInterface{
+class RecipeIngredient extends CRUD{
 
   function __construct(QueryBuilderParent ...$query){
     $sql = "INSERT INTO Recipe_Ingredient (recipe_id, ingredient_name) VALUES (:recipe_id , :ingredient_name)";
@@ -13,13 +12,55 @@ class RecipeIngredient extends CRUD implements CRUInterface{
     parent::__construct($query);
   }
 
-  function insert(\model\Model $model) : String{
-    $recipe_id = $model->getRecipeId();
-    $ingredient_name = $model->getIngredientName();
-    $this->stmt->bindParam(':recipe_id', $recipe_id);
-    $this->stmt->bindParam(':ingredient_name', $ingredient_name);
+  function insert(array $array, int $recipe_id) : String{
+    $sql = "INSERT INTO Recipe_Ingredient (recipe_id, ingredient_name) VALUES ";
 
-    $this->stmt->execute();
+
+    $i = 0;
+    /*foreach ($array as $value) {
+      if($i+1 != count($array)){
+        $sql .= "(:recipe_id$i, :ingredient_name$i),";
+        $i++;
+      }else{
+        $sql .= "(:recipe_id$i, :ingredient_name$i)";
+        $i++;
+      }
+    }*/
+
+    foreach ($array as $value) {
+      if($i+1 != count($array)){
+        $sql .= "(?,?),";
+        $i++;
+      }else{
+        $sql .= "(?,?)";
+        $i++;
+      }
+    }
+
+    \var_dump($sql);
+    \var_dump($array);
+
+    $this->stmt = \database\Database::getConnection()->prepare($sql);
+
+    /*$k = 0;
+    foreach ($array as  $value) {
+      $recipe_id = (int)$recipe_id;
+      \var_dump($recipe_id);
+      $this->stmt->bindParam(":recipe_id$k", $recipe_id);
+      $this->stmt->bindParam(":ingredient_name$k", $value);
+      var_dump(":ingredient_name$k");
+      \var_dump($k);
+      $k++;
+    }*/
+    $newArray = [];
+    foreach ($array as $value) {
+      $newArray[] = $recipe_id;
+      $newArray[] = $value;
+    }
+    \var_dump($newArray);
+
+    $this->stmt->execute($newArray);
+    \var_dump($this->stmt);
 
     return $this->stmt->errorcode();
   }
